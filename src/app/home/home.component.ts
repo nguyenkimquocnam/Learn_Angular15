ec55152e85a31caf9e244eda89c1cc6ffbfd4ce9
nguyenkimquocnam <nguyenkimquocnam@gmail.com>
2022-12-08 14:40:17 +0700

    ngFor Apply

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,17 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  public name = "Nam";
-  public age = 15;
-  public listName = ['John', 'Maria', 'Mario'];
+  public listFruits = [
+    { name: "Apple", price: 40000, bestSeller: true },
+    { name : "Orange", price: 35000, bestSeller: false},
+    { name : "Banana", price: -20500, bestSeller: true}
+  ];
 
   constructor() {}
 
   ngOnInit(): void {
-    console.log('List name = ', this.listName);
+    console.log('List Fruits = ', this.listFruits);
   }
 
-  public resetName(){
-    this.name = '';
-  }
 }
